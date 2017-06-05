fe06d1eb71db09b9eb5bdc8b6a38e89e1b3e1a44
Laura Hamilton <laurahamilton9@gmail.com>
2017-06-05 12:32:42 -0700

    add list elements and defined new array

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,25 @@ $(document).ready(function() {
     var colorInput = $("input#color").val();
     var animalInput= $("input#animal").val();
     var browserInput = $("input#browser").val();
-      console.log(monthInput, colorInput, animalInput, browserInput);
 
+    var formArray = [monthInput, colorInput, animalInput, browserInput];
+//create an array and add 4 items to array
 
-    $(".month").text(monthInput);
-    $(".color").text(colorInput);
-    $(".animal").text(animalInput);
-    $(".browser").text(browserInput);
+    console.log(formArray[1],formArray[0],formArray[2]);
+//shows and pulls the order results in the console, color, month, and animal, only reorders for the sake of the exercise
 
-// NOTE: Removed the show comand for now  $("#list").show();
-
+    var newArrayorder = [];
+//defines an empty new array
+    newArrayorder.push(formArray[1]);
+    newArrayorder.push(formArray[0]);
+    newArrayorder.push(formArray[2]);
+//lists out the 3 elements and appends them with the .push
+    console.log(newArrayorder);
+//shows the new array in the consule
+    $("#list").append("<li>" + newArrayorder[0] + "</li>");
+    $("#list").append("<li>" + newArrayorder[1] + "</li>");
+    $("#list").append("<li>" + newArrayorder[2] + "</li>");
+//defines the list class, elements, and it's new order from when it was defined by the original array elements
     event.preventDefault();
   });
 });
